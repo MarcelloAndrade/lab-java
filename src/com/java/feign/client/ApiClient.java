45b9acae320e3f0f705f150615fb142162589caa
marcellohfandrade <marcellohfandrade@gmail.com>
2020-02-18 18:09:52 -0300

    add classe exemplo FeignClient

---
+++ src/com/java/feign/client/ApiClient.java
@@ -0,0 +1,40 @@
+package com.java.feign.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.UUID;
+
+@FeignClient(url = "${path.url}", name = "name-client")
+public interface ApiClient {
+
+
+    @PostMapping(path = "/object")
+    Object createObject(
+            @RequestBody ObjectBody body,
+            @RequestHeader(value = "integrationToken", required = true) String integrationToken,
+            @RequestHeader(value = "login", required = false) String login,
+            @RequestHeader(value = "password", required = false) String password,
+            @RequestHeader(value = "token", required = false) String token);
+
+    @GetMapping(path = "/object/{objectUID}/{fields}-{base64}-{url}")
+    Object getObject(
+            @RequestHeader(value = "integrationToken", required = true) String integrationToken,
+            @RequestHeader(value = "login", required = false) String login,
+            @RequestHeader(value = "password", required = false) String password,
+            @RequestHeader(value = "token", required = false) String token,
+            @PathVariable("objectUID") UUID objectUID,
+            @RequestParam(value = "repository-full-name", required = false) String repositoryFullName);
+
+    @GetMapping(path = "/object/{modelUID}")
+    Object searchObject(
+            @RequestHeader(value = "integrationToken", required = true) String integrationToken,
+            @RequestHeader(value = "login", required = false) String login,
+            @RequestHeader(value = "password", required = false) String password,
+            @RequestHeader(value = "token", required = false) String token,
+            @PathVariable("modelUID") UUID modelUID,
+            @RequestParam(value = "fields") String fields);
+
+}
